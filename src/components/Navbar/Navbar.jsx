c80cc9091039aaa20ad94f4e6cdbbfd3c4b606c9
osmangony01 <mdosmangoni376@gmail.com>
2023-04-11 23:12:14 +0600

    add active link styling in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Navbar = () => {
     return (
         <nav className='nav'>
             <p className='title'>JobHub</p>
             <div>
-                <Link to="/">Home</Link>
-                <Link to="/applied-jobs">Applied Jobs</Link>
-                <Link to="/statistics">Statistics</Link>
-                <Link to="/blog">Blog</Link>
+                <ActiveLink to="/">Home</ActiveLink>
+                <ActiveLink to="/applied-jobs">Applied Jobs</ActiveLink>
+                <ActiveLink to="/statistics">Statistics</ActiveLink>
+                <ActiveLink to="/blog">Blog</ActiveLink>
             </div>
             <button className='btn-primary'><Link to="">Start Applying</Link></button>
         </nav>
